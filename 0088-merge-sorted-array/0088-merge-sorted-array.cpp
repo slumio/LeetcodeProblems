95678a69a47677a1f3840b2a78ec6da349dbe1bf
Deepak Mallah <dmallah474@gmail.com>
2025-08-27 11:08:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+        // so in this question i have prepared to follow ups means explain them brute force and then explain them how will u shift the array and then how will u do it
         int place=m+n-1;
 
         if(nums2.size()==0)return ;
@@ -10,28 +10,20 @@ public:
             nums1=nums2;
         }
 
-       int p=m-1;// last index where there is non zero element in nums1
     
-       while(p>=0)
-       {
-        nums1[place]=nums1[p];
-        place--;p--;
-       }
-      
-      
-      int i=n;
-      int j=0;
+      int i=m-1;
+      int j=n-1;
 
         
-        int start=0;
-        while(i<n+m||j<n)
+        int start=n+m-1;
+        while(start>=0)
         {
-            if(j>=n)nums1[start]=nums1[i++];
-            else if(i>=n+m)nums1[start]=nums2[j++];
-            else if(nums1[i]<nums2[j]){nums1[start]=nums1[i++];}
-            else {nums1[start]=nums2[j++];}
+            if(j<0)nums1[start]=nums1[i--];
+            else if(i<0)nums1[start]=nums2[j--];
+            else if(nums1[i]<nums2[j]){nums1[start]=nums2[j--];}
+            else {nums1[start]=nums1[i--];}
 
-            start++;
+            start--;
         }
 
 
