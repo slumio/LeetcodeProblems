2cf8ec2017e9d5da63add99fa5f2cd4d3c545f38
Deepak Mallah <dmallah474@gmail.com>
2025-08-29 15:19:36 +0530

    Time: 0 ms (100%), Space: 31.4 MB (16.85%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,9 +11,17 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-         if(!root)return 0;
+   
+    int countNodes(TreeNode* root, int l = 1, int r = 1) 
+    {
+        if (!root) return 0;
 
-         return countNodes(root->left)+countNodes(root->right)+1;
+        TreeNode *left = root, *right = root;
+        while (left = left->left)   ++l; 
+        while (right = right->right) ++r; 
+        
+        if (l == r) return (1 << l) - 1;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
